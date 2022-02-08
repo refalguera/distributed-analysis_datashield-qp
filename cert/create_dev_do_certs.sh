3a36c4edb40403c7c677efc743d0fd0b1d50d34c
Renata <renata.falguera@gmail.com>
2022-02-08 16:37:00 +0100

    Queue and CA updates with DNS and IP

--- cert/create_dev_do_certs.sh
+++ cert/create_dev_do_certs.sh
@@ -7,7 +7,7 @@ certnames=("queue" "opal")
 for certname in "${certnames[@]}"; do
   CURRENT="do_cert/$certname"
   echo $CURRENT
-  openssl req -config "${certname}cert.cnf" -days 365 -nodes -new -keyout $CURRENT.key -out $CURRENT.csr
+  openssl req -config "${certname}cert.cnf" -days 365 -nodes -new  -keyout $CURRENT.key -out $CURRENT.csr
   openssl ca -batch -notext -config "${certname}cert.cnf" -out $CURRENT.crt -infiles $CURRENT.csr 
   cat $CURRENT.crt $CURRENT.key > $CURRENT.pem
 done
